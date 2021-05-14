ef06ad07a40250978fdbad6edfde598c491e57bf
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2021-05-13 21:35:40 -0300

    change to css and a little more structure ( buttons, use of bootstrap )

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Formulario from './components/Formulario'
 import ListaDeNotas from './components/ListaDeNotas'
+import "./global.scss";
 
 function App() {
   return (
     <div className="App">
       <main>
         <Formulario />
+        <div className="traco" />
         <ListaDeNotas />
       </main>
     </div>
--- src/components/ListaDeNotas/index.jsx
+++ src/components/ListaDeNotas/index.jsx
@@ -2,9 +2,31 @@ import React from 'react';
 import CardNota from '../CardNota';
 
 function ListaDeNotas() {
+
+    const lista = [
+        {
+            title: "Nota 1",
+            text: "Texto 1"
+        },
+        {
+            title: "Nota 2",
+            text: "Texto 2"
+        },
+        {
+            title: "Nota 3",
+            text: "Texto 3"
+        },
+    ]
+
     return(
         <ul>
-            <CardNota />
+            {lista.map( (obj, index) => {
+                return(
+                    <li key={index}>
+                        <CardNota  title={obj.title} text={obj.text} />
+                    </li>
+                );
+            })}
         </ul>
     );
 }
